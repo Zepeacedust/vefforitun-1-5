422f5be142a9e2c220720f5e3d8c51cfc61fa274
Arnor <arnor0903@gmail.com>
2019-10-08 12:07:00 +0000

    gleymdi að setja inn nokkur ný files

--- app.py
+++ app.py
@@ -4,54 +4,40 @@ from flask import Flask, render_template as rend, session
 
 app = Flask(__name__)
 
-with open("frettir.txt") as frett_file:
-	frettir = ast.literal_eval(frett_file.read())
+with open("bud.txt") as bud_file:
+	hlutir = ast.literal_eval(bud_file.read())
 
 app.config['SECRET_KEY'] = secrets.token_hex(256)
 
-@app.route('/')
-def index():
-	return '<h3><a href="/on">Set session</a></h3><h3><a href="/off">Delete session</a></h3><h3><a href="/check">Check session</a></h3>'
-
-# Setjum session í gang, 'hilmir' og 'GALVEZ' eru bæði valkvæð gildi sem þú ákveður sjálf / ur
-@app.route('/on')
-def sessionon():
-    session['hilmir'] = 'GALVEZ'
-    return '<h3>Session ON</h3><h3><a href="/">Home</a></h3>'
-
-# Eyðum session
-@app.route('/off')
-def sessionoff():
-    if 'hilmir' in session:
-        session.pop('hilmir', None)
-        return '<h3>Session poped</h3><h3><a href="/">Home</a></h3>'
-    else:
-        return '<h3>Session was not set</h3><h3><a href="/">Home</a></h3>'
-
-# Athugum hvort session 'hilmir' sé í gangi
-@app.route('/check')
+@app.route('/karfa')
 def checksession():
-    if 'hilmir' in session:
-        print(session['hilmir']) # Debug, prentum gildi session í cmd
-        return '<h3>ON</h3><h3><a href="/">Home</a></h3>'
-    else:
-        return '<h3>OFF</h3><h3><a href="/">Home</a></h3>'
+	output = []
+	for x in session["karfa"]:
+		output.append(hlutir[x])
+	return rend("karfa.html", karfa=output)
 
+@app.route("/add/<hlut>")
+def add(hlut):
+	karfa = session["karfa"]
+	karfa.append(int(hlut))
+	session["karfa"] = karfa
+	print(session["karfa"])
+	return index()
 
-if __name__ == "__main__":
-	app.run(debug=True)
-"""
+@app.route("/empty")
+def empty():
+	session["karfa"] = []
+
+	
 @app.route('/')
 def index():
-	return rend("frettir.html", title="index", frettir=frettir.keys())
+	if not "karfa" in session:
+		print("reset")
+		session["karfa"] = []
+	return rend("bud.html", title="index", hlutir=hlutir.keys())
 
 
-@app.route("/<frett>")
-def frett(frett):
-	if frett in frettir.keys():
-		return rend("frett.html", title=frettir[frett][0], content=frettir[frett][1], pic=frett)
-	else:
-		return page_not_found(None)
+
 
 
 @app.errorhandler(404)
@@ -60,4 +46,4 @@ def page_not_found(e):
 
 
 if __name__ == "__main__":
-	app.run(debug=True)"""
+	app.run(debug=True)
